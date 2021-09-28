62a41e935465eb9f417aa223571919ac0e42fe3d
pc0riginal <pc.pramukh99@gmail.com>
2021-09-28 09:19:09 +0530

    Javascript Basics added

---
+++ sandBox.js
@@ -0,0 +1,46 @@
+/*
+today class
+install vscode for coding 
+what is server ? install live server package in vscode
+github for public code
+first html and live server
+where do we add js to html page  - add external
+alert() => first hello world program
+semicolon after every line - optional
+inspect element - console.log
+what is variable ? - camle case - let,const,var
+comments
+dataTypes => [number,string,boolean,null,undefined,object,symbol] 
+lossly data type in js
+string - concatenation,getting char,string length,methods
+some string methods - indexof,replace,substr,lastindexof,slice
+numbers - math op [+,-,/,*,**,%] - order of operation - BODMAS
+*/
+
+//console.log("hello world!")
+console.log("hey I am learning JS")
+firstName = 'paras'
+
+// first_name = 'jack'
+// console.log(first_name)
+
+var firstName = 'patel'
+firstName = 123
+console.log(firstName)
+
+// firstName = 'coder'
+// console.log(firstName)
+
+const age = 23
+console.log(age)
+
+let likes ;
+console.log(typeof(likes))
+
+
+
+// let radius = 20
+// const pi = 3.14
+// let area = pi*radius**2
+// console.log(10%3)
+// console.log(area)
